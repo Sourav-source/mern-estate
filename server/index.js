361b9d3cec796cc9ead256f05256866337771ae3
Sourav-source <satpatisourav126@gmail.com>
2023-11-04 10:59:56 +0530

    Connect to Database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
+const { configDotenv } = require("dotenv");
 const express = require("express");
+const mongoose = require("mongoose");
+configDotenv()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to DB");
+}).catch(err=>console.log(err))
 
 const app = express();
 
-const PORT = 3000
+const PORT = 3000;
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT} LoL`);
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} LoL`);
+});
